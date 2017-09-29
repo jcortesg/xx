8404eff724b598fbf9f8af0bff5b5a0ae960de13
jhon <jhone.cortesg@gmail.com>
2017-09-29 12:51:09 -0500

    graphy

--- assets/js/src/indicators/module.jsx
+++ assets/js/src/indicators/module.jsx
@@ -20,12 +20,26 @@ export default class Application extends React.Component {
 
     return(
       <div>
-        <h2>Indicadores</h2>
-        <XYPlot width={400} height={300}><XAxis/><YAxis/>
-          <HorizontalGridLines />
-          <VerticalGridLines />
-          <LineMarkSeries data={data} />
-        </XYPlot>
+        <div class="row">
+          <div class="col-md-3">
+            <h4>Indicadores</h4>
+            ..Filtros
+          </div>
+          <div class="col-md-9">
+            <h2 class="text-center">Certificaciones de calidad de las empresas TI</h2>
+
+            <p class="text-center">
+              Número de empresas TI que tienen al menos un modelo de la calidad
+              implementado en un periodo de tiempo determinado
+            </p>
+            <hr/>
+            <XYPlot width={400} height={300}><XAxis/><YAxis/>
+              <HorizontalGridLines />
+              <VerticalGridLines />
+              <LineMarkSeries data={data} />
+            </XYPlot>
+          </div>
+        </div>
       </div>
     )
   }
